a41e09f7180f0b8dc89cdbb91f9d84f1c5e9d54b
Scrooge <duckmoney@protonmail.com>
2021-03-02 23:52:15 +0100

    rebrand to Duck

--- src/views/Home/components/CakeStats.tsx
+++ src/views/Home/components/CakeStats.tsx
@@ -26,15 +26,15 @@ const CakeStats = () => {
   const TranslateString = useI18n()
   const totalSupply = useTotalSupply()
   const burnedBalance = useBurnedBalance(getCakeAddress())
-  const farms = useFarms();
-  const duckPrice = usePriceCakeBusd();
-  const circSupply = totalSupply ? totalSupply.minus(burnedBalance) : new BigNumber(0);
-  const cakeSupply = getBalanceNumber(circSupply);
-  const marketCap = duckPrice.times(circSupply);
+  const farms = useFarms()
+  const duckPrice = usePriceCakeBusd()
+  const circSupply = totalSupply ? totalSupply.minus(burnedBalance) : new BigNumber(0)
+  const cakeSupply = getBalanceNumber(circSupply)
+  const marketCap = duckPrice.times(circSupply)
 
-  let duckPerBlock = 0;
-  if(farms && farms[0] && farms[0].duckPerBlock){
-    duckPerBlock = new BigNumber(farms[0].duckPerBlock).div(new BigNumber(10).pow(18)).toNumber();
+  let duckPerBlock = 0
+  if (farms && farms[0] && farms[0].duckPerBlock) {
+    duckPerBlock = new BigNumber(farms[0].duckPerBlock).div(new BigNumber(10).pow(18)).toNumber()
   }
 
   return (
@@ -61,7 +61,9 @@ const CakeStats = () => {
         </Row>
         <Row>
           <Text fontSize="14px">{TranslateString(540, 'New QUACK/block')}</Text>
-          <Text bold fontSize="14px">{duckPerBlock}</Text>
+          <Text bold fontSize="14px">
+            {duckPerBlock}
+          </Text>
         </Row>
       </CardBody>
     </StyledCakeStats>
